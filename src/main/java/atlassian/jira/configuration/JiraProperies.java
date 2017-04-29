d07d987d185c7d3b7eb450cc9e579d9c2a88eb77
dodoni <dodoni@localhost>
2017-04-30 01:47:32 +0300

    added jira properties

---
+++ src/main/java/atlassian/jira/configuration/JiraProperies.java
@@ -0,0 +1,28 @@
+package atlassian.jira.configuration;;
+
+public class JiraProperies {
+	
+	
+    private String jiraUrl = "";
+    private String username = "";
+    private String password = "";
+	public String getJiraUrl() {
+		return jiraUrl;
+	}
+	public void setJiraUrl(String jiraUrl) {
+		this.jiraUrl = jiraUrl;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
